9905718bc25e2a6428fec64ca7e01c52b762b7b9
Pat LaChapelle <31046873+physicslifter@users.noreply.github.com>
2026-03-22 18:11:39 -0400

    Added tests for portfolio

---
+++ tests/test_portfolio.py
@@ -0,0 +1,11 @@
+'''
+Scripts for testing portfolio class
+'''
+from Portfolio import Portfolio
+from pdb import set_trace as st
+
+test_portfolio = 1
+
+if test_portfolio == True:
+    portfolio = Portfolio(model_folder = "../Data/Models/Model12")
+    st()
